0447458e394c2671a79abfb8130ea7e9622d52ea
Rafiq Hilali <rafiq@lambertlabs.com>
2023-01-12 19:55:18 +0000

    chore: update ugettext_lazy to gettext_lazy

--- src/sphinxdoc/models.py
+++ src/sphinxdoc/models.py
@@ -4,7 +4,7 @@ Models for django-sphinxdoc.
 """
 from django.conf import settings
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 
 from sphinxdoc.validators import validate_isdir
